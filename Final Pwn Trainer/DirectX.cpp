d99a9beb3ad297ea2884ac9ca75fb5fc4d8761ec
brckndrt <bdneidert@gmail.com>
2019-12-04 13:42:30 -0800

    Removed Unused Functions (DrawString/WorldToScreen)

--- Final Pwn Trainer/DirectX.cpp
+++ Final Pwn Trainer/DirectX.cpp
@@ -79,36 +79,6 @@ wchar_t teleOptions[8][255] =   { L"                         Town",
 wchar_t godOptions[2][255] =    { L"                         Off", 
 								  L"                          On" };
 
-void DrawString(char* String, int x, int y, int a, int r, int g, int b, ID3DXFont* font)
-{
-	RECT FontPos;
-	FontPos.left = x;
-	FontPos.top = y;
-	font->DrawTextA(0, String, strlen(String), &FontPos, DT_NOCLIP, D3DCOLOR_ARGB(a, r, g, b));
-}
-
-bool WorldToScreen(vec3 pos, vec2& screen, float matrix[16], int windowWidth, int windowHeight)
-{
-	//Matrix-vector Product, multiplying world(eye) coordinates by projection matrix = clipCoords
-	vec4 clipCoords;
-	clipCoords.x = pos.x * matrix[0] + pos.y * matrix[1] + pos.z * matrix[2] + matrix[3];
-	clipCoords.y = pos.x * matrix[4] + pos.y * matrix[5] + pos.z * matrix[6] + matrix[7];
-	clipCoords.z = pos.x * matrix[8] + pos.y * matrix[9] + pos.z * matrix[10] + matrix[11];
-	clipCoords.w = pos.x * matrix[12] + pos.y * matrix[13] + pos.z * matrix[14] + matrix[15];
-
-	if (clipCoords.w < 0.1f)
-		return false;
-
-	//perspective division, dividing by clip.W = Normalized Device Coordinates
-	vec3 NDC;
-	NDC.x = clipCoords.x / clipCoords.w;
-	NDC.y = clipCoords.y / clipCoords.w;
-	NDC.z = clipCoords.z / clipCoords.w;
-
-	screen.x = (windowWidth / 2 * NDC.x) + (NDC.x + windowWidth / 2);
-	screen.y = -(windowHeight / 2 * NDC.y) + (NDC.y + windowHeight / 2);
-	return true;
-}
 
 void DirectxFunctions::DirectXInit(HWND hwnd)
 {
